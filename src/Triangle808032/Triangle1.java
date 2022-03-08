37ee10ebf36b59ef31ab5d09dab9b23f6a5cb482
Vrunda1 <thakervrunda181@gmail.com>
2022-03-08 21:55:56 +0000

    Triangle

---
+++ src/Triangle808032/Triangle1.java
@@ -0,0 +1,14 @@
+package Triangle808032;
+
+public class Triangle1 {
+    public  static  void main(String [] args)
+    {
+        System.out.println("  * * * * * * * * * ");
+        System.out.println("    * * * * * * *   ");
+        System.out.println("     * * * * *      ");
+        System.out.println("       * * *        ");
+        System.out.println("        * *         ");
+        System.out.println("         *          ");
+
+    }
+}
